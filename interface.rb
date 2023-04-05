f63cb67adb7da926305721991afaa9d0ff903de9
kaltag <alexx30rus@gmail.com>
2023-04-05 19:49:17 +0400

    update interface

--- interface.rb
+++ interface.rb
@@ -7,21 +7,27 @@ class Interface
     @diller = User.new('Diller')
     # TODO: CHECK NEW CARD
     @cards = Card.new
+    @user_score = Score.new
+    @diller_score = Score.new
     start_game
   end
 
   def start_game
-    @user.add_hand(@cards)
-    @diller.add_hand(@cards)
+    2.times { @user.add_hand(@cards) }
+    2.times { @diller.add_hand(@cards) }
     puts @user.hand
-    # show user points
+    puts 'You points: '
+    @user_score.point(@user.hand)
+    puts @user.score = @user_score.score
 
-    # TODO: DELETE
-    puts '----------------'
-    puts @diller.hand
+    # TODO: DELETE?
+
+    @diller_score.point(@diller.hand)
+    @diller.score = @diller_score.score
 
     @user.remove_money
     @diller.remove_money
+
     user_turn
   end
 
@@ -29,16 +35,44 @@ class Interface
     puts '1 - Add card'
     puts '2 - Open hands'
     # puts '3 - skip/end turn'
+
+    case gets.chomp.to_i
+    when 1
+      @user.add_hand(@cards)
+      puts @user.hand
+      puts 'You points: '
+      @user_score.score = 0
+
+      @user_score.point(@user.hand)
+      puts @user.score = @user_score.score
+      puts @user_score.score
+
+      diller_turn
+    when 2
+      diller_turn
+    end
   end
 
-  # TODO: temporary value
-  @score = 0
-
   def diller_turn
-    if @score >= 17
-      # end turn
+    unless @diller.score >= 17
+      @diller_score.score = 0
+      @diller.add_hand(@cards)
+      @diller_score.point(@diller.hand)
+      # @diller.score = @diller_score.score
+    end
+    end_game
+  end
+
+  def end_game
+    puts @user.hand
+    puts @user_score.score
+    puts '----------------'
+    puts @diller.hand
+    puts @diller_score.score
+    if @user_score.score > @diller_score.score
+      puts 'You win'
     else
-      # add card
+      puts 'You lose'
     end
   end
 end
