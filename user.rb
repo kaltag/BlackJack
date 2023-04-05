5f8bf1a840568ce280efc5b89834c107d0edded1
kaltag <alexx30rus@gmail.com>
2023-04-05 12:16:07 +0400

    make user with money

---
+++ user.rb
@@ -0,0 +1,16 @@
+class User
+  attr_reader :money, :name
+  def initialize(name)
+    @money = 100
+    @name = name
+  end
+
+  def add_money
+    @money += 10
+  end
+
+  def remove_money
+    @money -= 10
+  end
+
+end
