c8b910fbffd5514f598ba2f2b60949083b91b6a5
kaltag <alexx30rus@gmail.com>
2023-04-05 12:40:25 +0400

    add interface

---
+++ interface.rb
@@ -0,0 +1,35 @@
+class Interface
+  def start
+    puts "What's your name?"
+    # TODO: do validation
+    name = gets.chomp
+    u = User.new(name)
+    d = User.new(diller = "Diller")
+    start_game
+  end
+
+  def start_game
+    # add 2 cards to user
+    # add 2 cards to diller
+    # show user points
+    # user and diller remove_money
+    user_turn
+  end
+
+  def user_turn
+    puts "1 - Add card"
+    puts "2 - Open hands"
+    # puts '3 - skip/end turn'
+  end
+
+  # TODO: temporary value
+  score = 0
+
+  def diller_turn
+    if score >= 17
+      # end turn
+    else
+      # add card
+    end
+  end
+end
---
+++ main.rb
@@ -0,0 +1,4 @@
+require_relative 'interface'
+require_relative 'user'
+
+Interface.new.start
